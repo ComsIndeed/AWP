e60c3a0e6ef2fe9090e2291d72282dbce94cc99d
ComsIndeed <enderdragonmaster8059@gmail.com>
2024-07-22 01:32:41 +0800

    classes.ts is good. arithmetic.ts is a mess

--- src/lib/AWP/Math/classes.ts
+++ src/lib/AWP/Math/classes.ts
@@ -2,37 +2,85 @@ export abstract class Answer {
   value: number;
   unit?: string;
 
-  constructor(answer: { value: number; unit: string }) {
+  constructor(answer: { value: number; unit?: string }) {
     this.value = answer.value;
     this.unit = answer.unit;
   }
 }
 
-export abstract class Question {
-  header: QuestionHeader;
-  question: QuestionData;
+// Question Class (Modified)
+export abstract class Question<
+  Topic extends string,
+  Difficulty extends number,
+  QuestionItemObject,
+  QuestionAnswerType extends string,
+  QuestionAnswerExpects
+> {
+  header: QuestionHeader<Topic, Difficulty>;
+  item: QuestionItem<Topic, QuestionItemObject>;
+  answer: QuestionAnswer<Topic, QuestionAnswerType, QuestionAnswerExpects>;
 
-  constructor(header: QuestionHeader, question: QuestionData) {
-    this.header = header;
-    this.question = question;
+  constructor(
+    topic: Topic,
+    difficulty: Difficulty,
+    questionItemObject: QuestionItemObject,
+    questionAnswerType: QuestionAnswerType,
+    questionAnswerExpects: QuestionAnswerExpects,
+    questionItemText?: QuestionItem<Topic, QuestionItemObject>["asText"]
+  ) {
+    this.header = { topic, difficulty };
+    this.item = {
+      topic,
+      asObject: questionItemObject,
+      asText: questionItemText,
+    };
+    this.answer = {
+      forTopic: topic,
+      type: questionAnswerType,
+      expects: questionAnswerExpects,
+    };
   }
 }
 
-export interface QuestionHeader {
-  topic: string;
-  difficulty: number;
+export interface QuestionHeader<
+  Topic extends string,
+  Difficulty extends number
+> {
+  topic: Topic;
+  difficulty: Difficulty;
 }
 
-interface QuestionObjectFormat {
-  [key: string]: string | number | string[] | number[];
+export interface QuestionItem<Topic extends string, AsObject> {
+  topic: Topic;
+  asText?: string;
+  asObject?: AsObject;
 }
 
-export interface QuestionData {
-  type: "choice" | "blank";
-  textFormat?: string;
-  objectFormat: QuestionObjectFormat;
-  choices?: string[] | number[];
-  answer: string | number;
+export interface QuestionAnswer<
+  ForTopic extends string,
+  Type extends string,
+  Expects
+> {
+  forTopic: ForTopic;
+  type: Type;
+  expects: Expects;
 }
 
-export abstract class Term {}
+export type AlgebraicExpression = Term[];
+export class Term {
+  coefficient: number;
+  variables: Record<string, number>;
+
+  static newExpression(
+    terms: [number, Record<string, number> | {} | void][]
+  ): AlgebraicExpression {
+    return terms.map((termItem) => {
+      return new Term(termItem[0], termItem[1] ? termItem[1] : undefined);
+    });
+  }
+
+  constructor(coefficient: number, variables: Record<string, number> | void) {
+    this.coefficient = coefficient;
+    this.variables = variables || {};
+  }
+}
--- src/lib/AWP/Math/topics/arithmetic.ts
+++ src/lib/AWP/Math/topics/arithmetic.ts
@@ -1,24 +1,52 @@
-import { Question, type QuestionData, type QuestionHeader } from "../classes";
+import { Question, type QuestionAnswer, type QuestionItem, type QuestionHeader, Term } from "../classes";
 
 class Arithmetic {
-  static createQuestion(difficulty: number): ArithmeticQuestion {
-    return void;
-  }
+
+  // // This should return an instance of ArithmeticQuestion
+  // static createQuestion(difficulty: number): ArithmeticQuestion {
+  //   return void;
+  // }
 }
 
-// For doing actual arithmetic calculations; Backend
-class ArithmeticOperations {
-  static getResult()
-}
-
-class ArithmeticQuestion extends Question {
+// creating a new instance of ArithmeticQuestion must create a question
+class ArithmeticQuestion extends Question<"arithmetic", Difficulty, ArithmeticQuestionItem['asObject'], ArithmeticQuestionAnswer['type'], ArithmeticQuestionAnswer['expects']> {
+  
   createChoices() {}
 
-  constructor(options: {type: "choice" | "blank" ,difficulty: number}) {
+  constructor(options: {type: "choice" | "blank" , difficulty: number}) {
     const headerData: QuestionHeader = { topic: "arithmetic", difficulty: options.difficulty };
-    const questionData: QuestionData = {
-      
+    const questionItem: QuestionItem = {
+      // TODO : We need to create questions here
     }
-    super(headerData, questionData);
+    const questionAnswer: QuestionAnswer = {
+      // TODO : We need to create the answers here too
+    }
+    super(headerData, questionItem, questionAnswer);
   }
 }
+
+interface ArithmeticQuestionItem extends QuestionItem<"arithmetic", Term[]> {}
+interface ArithmeticQuestionAnswer extends QuestionAnswer<"topic", "blank" | "choices", Term[]> {}
+
+// ==============================
+
+// TODO : Make a way to make questions
+const exampleQuestion: {header: QuestionHeader, item: ArithmeticQuestionItem, answer: ArithmeticQuestionAnswer} = {
+  header: {
+    difficulty: 124,
+    topic: "arithmetic"
+  },
+  item: {
+    topic: "arithmetic",
+    asObject: 
+  }
+}
+
+// ! INFO : Process of creating questions
+const newArithmeticQuestion = new ArithmeticQuestion({
+  difficulty: 102,
+  type: "choice"
+}) // returns an 
+
+
+
